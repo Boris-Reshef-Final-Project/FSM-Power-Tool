c5abb66582e5a52be072239379e7df61e1e80956
Boris <boris.karasov@s.afeka.ac.il>
2024-01-31 14:21:32 +0200

    added vhd code that's used in most state machines

--- kis2vhd.cpp
+++ kis2vhd.cpp
@@ -1,9 +1,47 @@
 
+#include <fstream>
 #include <iostream>
+#include <string>
+
+
+
 int main() 
 {   
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::string SourcePath, DestPath;
+	
+	//C:/Final Project/Parser Prototype 1/Parser/Parser/btat.txt
+
+	std::ifstream source("E:/Final Project/Project Files/bbsse.kis");    
+	std::ofstream dest("E:/Final Project/Project Files/dest.txt");    
+	if (!source || !dest) {
+
+		std::cerr << "Error opening files!" << std::endl;
+		return 1;   
+	}
+
+	std::string word; 
+
+
+
+/*
+	if (source >> word && source >> word) {
+		dest << word << std::endl;
+	}
+
+	//std::getline(source, word);
+
+	if (source >> word && source >> word) {
+		dest << word << std::endl;
+	}
+	*/
+
+	dest << std::endl << "library ieee;\nuse ieee.std_logic_1164.all;\nuse ieee.std_logic_arith.all;\nuse ieee.std_logic_unsigned.all;\n\nentity state_machine is\nport(";
+if (source >> word ) {
+		dest << word << std::endl;
+	}
+
+	return 0; 
+
 
     
 }
