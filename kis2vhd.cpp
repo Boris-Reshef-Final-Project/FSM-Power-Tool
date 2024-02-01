c7518cf86abda1dcd8f301a5edee0ecc3d7f065c
Boris <boris.karasov@s.afeka.ac.il>
2024-02-01 13:29:15 +0200

    added if statement for .i, added clk, rst, using namespace std

--- kis2vhd.cpp
+++ kis2vhd.cpp
@@ -3,14 +3,14 @@
 #include <iostream>
 #include <string>
 
-
+using namespace std;
 
 int main() 
 {   
     std::string SourcePath, DestPath;
 	
 	//C:/Final Project/Parser Prototype 1/Parser/Parser/btat.txt
-	
+
 
 	std::ifstream source("E:/Final Project/Project Files/bbsse.kis");    
 	std::ofstream dest("E:/Final Project/Project Files/dest.txt");    
@@ -36,15 +36,41 @@ int main()
 	}
 	*/
 
-	dest << std::endl << "library ieee;\nuse ieee.std_logic_1164.all;\nuse ieee.std_logic_arith.all;\nuse ieee.std_logic_unsigned.all;\n\nentity state_machine is\nport(";
+	dest << std::endl << "library ieee;\nuse ieee.std_logic_1164.all;\nuse ieee.std_logic_arith.all;\nuse ieee.std_logic_unsigned.all;\n\nentity state_machine is\nport(\nclk\t\t\t:in std_logic;\nrst\t\t\t:in std_logic;";
+/*
 if (source >> word ) {
 		dest << word << std::endl;
 	}
+*/
+
+if (source.is_open() && dest.is_open()) {
+        string line;
+        while (getline(source, line)) {
+            // Check if the line contains ".i 7"
+            size_t found = line.find(".i 7");
+            if (found != string::npos) {
+                // Extract the number following ".i"
+                size_t startIndex = found + 3; // Skip ".i "
+                size_t endIndex = line.find_first_not_of("0123456789", startIndex); // Find first non-digit character
+                string numberString = line.substr(startIndex, endIndex - startIndex); // Extract the number as a string
+                int number = stoi(numberString); // Convert the string to an integer
+                // Write to destination file
+                dest << "\ninputs\t\t\t: in std_logic_vector(" << number << " downto 0);" << endl;
+            }
+        }
+        // Close files
+        source.close();
+        dest.close();
+
+
+
+
 
 	return 0; 
 
 
     
+}
 }
 
 
