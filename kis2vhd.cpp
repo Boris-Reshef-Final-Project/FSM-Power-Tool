39f78235ba520883779ee4ba789b663bddbab66d
Boris <boris.karasov@s.afeka.ac.il>
2024-05-26 16:37:37 +0300

    updated Fill_state_product with reshef

--- kis2vhd.cpp
+++ kis2vhd.cpp
@@ -10,23 +10,25 @@
 
 using namespace std;
 
-string Original_Reset_value;        // Original_Reset_value          -> (Fill_state_product)
-int input,output,products,states;   // products is (מספר שורות) - 1;-> (MakeIODecleration)
-                                    // input,output                  -> (MakeIODecleration)
-                                    // states                        -> (MakeTypeState)
+string Original_Reset_value;         // Original_Reset_value          -> (Fill_state_product)
+int input, output, products, states; // products is (מספר שורות) - 1;-> (MakeIODecleration)
+                                     // input,output                  -> (MakeIODecleration)
+                                     // states                        -> (MakeTypeState)
+stringstream type_state;
+vector<string> State_list;
 
-
-int  KissFiles2Vhd(int CfsmAmount, ifstream &source, ofstream &dest);
+int KissFiles2Vhd(int CfsmAmount, ifstream &source, ofstream &dest);
 void MakeIODecleration(ifstream &source, ofstream &dest);
 void MakeTypeState(ifstream &source, ofstream &dest);
 void FSM2Process(int j, ofstream &dest);
 void Fill_state_product(ifstream &source, ofstream &dest);
-bool isStringInVector(const string& str, const vector<string>& vec);
-int  find_cfsm(string state);
+bool isStringInVector(const string &str, const vector<string> &vec);
+int find_cfsm(string state);
 void Optimiser_Axe(ifstream &source, vector<vector<string>> &cfsm);
-void Optimiser_Min_trans_prob(ifstream &source, vector<vector<string>> &cfsm);
+void Optimiser_Min_trans_prob(vector<vector<string>> &cfsm);
 
-struct state_product {
+struct state_product
+{
     string x;
     string cs;
     string ns;
@@ -36,10 +38,6 @@ struct state_product {
 vector<state_product> stateProducts;
 
 vector<vector<string>> cfsm; // Vector to store the states of each cfsm
-
-
-
-        
 
 int main()
 {
@@ -62,15 +60,12 @@ int main()
     }
 
     int CfsmAmount = 2;
-    //cout << "insert amount of cfsm's :" << endl;
-    //cin >> CfsmAmount;
+    // cout << "insert amount of cfsm's :" << endl;
+    // cin >> CfsmAmount;
 
     // This is an assignment for prototyping. delete later!
-    //cfsm.push_back({"st0", "st1", "st2", "st3", "st4", "st5", "st6"});
-    //cfsm.push_back({"st7", "st8", "st9", "st10", "st11", "st12"});
-
-    
-
+    // cfsm.push_back({"st0", "st1", "st2", "st3", "st4", "st5", "st6"});
+    // cfsm.push_back({"st7", "st8", "st9", "st10", "st11", "st12"});
 
     KissFiles2Vhd(CfsmAmount, source, dest); // Preform the parsing process
 
@@ -80,8 +75,6 @@ int main()
 
     return 0;
 }
-
-
 
 int KissFiles2Vhd(int CfsmAmount, ifstream &source, ofstream &dest) // Main Parser function - Convert Kiss to Vhd
 {
@@ -103,32 +96,29 @@ int KissFiles2Vhd(int CfsmAmount, ifstream &source, ofstream &dest) // Main Pars
     dest << "\narchitecture arc_state_machine of state_machine is" << endl;
     dest << "\tsignal z\t\t: std_logic_vector(" << CfsmAmount - 1 << " downto 0);" << endl;
 
-    
+    cout << "Now starting Fill_state_product" << endl;
+    Fill_state_product(source, dest); // state_list = ("st0,st1...,stn")
+    cout << "Now Finished Fill_state_product" << endl;
 
     MakeTypeState(source, dest); // type state is (st0, st1, st2,..., st12);
                                  // signal st : state;
-    dest << "begin" << endl << endl;
-                                 
-   
-    
-    cout << "Now starting Fill_state_product"<< endl;
-    Fill_state_product(source, dest);
-    cout << "Now Finished Fill_state_product"<< endl;
+    dest << "begin" << endl
+         << endl;
 
     int Opt;
-    cout << "Choose Optimiser: 1 - Optimiser_Axe, 2 - Optimiser_Min_trans_prob"<< endl;
+    cout << "Choose Optimiser: 1 - Optimiser_Axe, 2 - Optimiser_Min_trans_prob" << endl;
     cin >> Opt;
-    if (Opt ==1)
-    Optimiser_Axe(source, cfsm); // Located After (Fill_state_product) && Before (FSM2Process) √√√√√√√√√
+    if (Opt == 1)
+        Optimiser_Axe(source, cfsm); // Located After (Fill_state_product) && Before (FSM2Process) √√√√√√√√√
     else
-    Optimiser_Min_trans_prob(source, cfsm);
-
+        Optimiser_Min_trans_prob(cfsm);
 
     int j;
     for (j = 0; j < CfsmAmount; j++)
     {
-        dest << "\n\t" << "cfsm" << j << ": process(clk(" << j << "), rst) begin\n" << endl;
-        dest << "\t\t" << "if(rst = '1') then"<< endl;
+        dest << "\n\t" << "cfsm" << j << ": process(clk(" << j << "), rst) begin\n"
+             << endl;
+        dest << "\t\t" << "if(rst = '1') then" << endl;
         dest << "\t\t\t" << "st\t<=\tst0;" << endl;
         dest << "\t\t\t" << "z <= (" << find_cfsm("st0") << " => '1', others => '0');" << endl;
         dest << "\t\t" << "elsif falling_edge(clk(" << j << ")) then" << endl;
@@ -139,15 +129,13 @@ int KissFiles2Vhd(int CfsmAmount, ifstream &source, ofstream &dest) // Main Pars
         FSM2Process(j, dest);
 
         dest << "\t\t" << "end if;" << endl;
-        dest << "\t" <<"end process cfsm" << j << ";\n\n";
+        dest << "\t" << "end process cfsm" << j << ";\n\n";
     }
     dest << "end arc_state_machine;" << endl;
     return 0;
 }
 
-
-
-void MakeIODecleration(ifstream &source, ofstream &dest) // 
+void MakeIODecleration(ifstream &source, ofstream &dest) //
 {
     if (source.is_open() && dest.is_open())
     {
@@ -157,7 +145,7 @@ void MakeIODecleration(ifstream &source, ofstream &dest) //
             size_t i_pos = line.find(".i ");
             size_t o_pos = line.find(".o ");
             size_t p_pos = line.find(".p ");
-
+            size_t s_pos = line.find(".s ");
             // Handle ".i" case
             if (i_pos != string::npos)
             {
@@ -167,7 +155,6 @@ void MakeIODecleration(ifstream &source, ofstream &dest) //
                 int number = stoi(numberString) - 1;
                 input = stoi(numberString) - 1;
                 dest << "\t" << "x\t\t: in\tstd_logic_vector(" << number << " downto 0);" << endl;
-
             }
 
             // Handle ".o" case
@@ -181,35 +168,6 @@ void MakeIODecleration(ifstream &source, ofstream &dest) //
                 dest << "\t" << "y\t\t: out\tstd_logic_vector(" << number << " downto 0)" << endl;
             }
 
-
-             if (p_pos != string::npos)
-            {
-                size_t startIndex = p_pos + 3; // Skip ".p "
-                size_t endIndex = line.find_first_not_of("0123456789", startIndex);
-                string numberString = line.substr(startIndex, endIndex - startIndex);
-                
-                products = stoi(numberString) - 1;
-                
-            }
-        }
-    }
-}
-
-
-
-void MakeTypeState(ifstream &source, ofstream &dest) // Write the values of the enum type state
-{
-    if (source.is_open() && dest.is_open())
-    {
-        source.clear();
-        source.seekg(0, ios::beg); // Return to the beginning of the file
-
-        string line;
-        while (getline(source, line))
-        {
-            size_t s_pos = line.find(".s ");
-
-            // Handle ".s" case
             if (s_pos != string::npos)
             {
                 size_t startIndex = s_pos + 3; // Skip ".s "
@@ -218,33 +176,52 @@ void MakeTypeState(ifstream &source, ofstream &dest) // Write the values of the 
                 int number = stoi(numberString);
 
                 states = stoi(numberString);
+            }
 
+            if (p_pos != string::npos)
+            {
+                size_t startIndex = p_pos + 3; // Skip ".p "
+                size_t endIndex = line.find_first_not_of("0123456789", startIndex);
+                string numberString = line.substr(startIndex, endIndex - startIndex);
 
-                // Write type declaration
-                dest << "\t" << "type state is (";
-                for (int i = 0; i < number; ++i)
-                {
-                    dest << "st" << i;
-                    if (i < number - 1)
-                    {
-                        dest << ", ";
-                    }
-                }
-                dest << ");" << endl;
-
-                // Write signal declaration
-                dest << "\tsignal st : state;" << endl;
+                products = stoi(numberString) - 1;
             }
         }
     }
 }
 
+void MakeTypeState(ifstream &source, ofstream &dest) // Write the values of the enum type state
+{
+    if (source.is_open() && dest.is_open())
+    {
+
+       
+       
+            type_state << "\t" << "type state is (";
+            for (int i = 0; i < states; ++i)
+            {
+
+                type_state << State_list[i];
+                if (i < states - 1)
+                {
+
+                    type_state << ", ";
+                }
+            }
+
+            type_state << ");" << endl;
+            dest << type_state.str();
+
+            // Write signal declaration
+            dest << "\tsignal st : state;" << endl;
+        }
+    }
 
 
 void FSM2Process(int j, ofstream &dest)
 {
     dest << "\t\t\t" << "case st is" << endl;
-    for(int i = 0; i < cfsm[j].size(); i++) // Iterate over all the states of this specific cfsm
+    for (int i = 0; i < cfsm[j].size(); i++) // Iterate over all the states of this specific cfsm
     {
         dest << "\t\t\t\t" << "when " << cfsm[j][i] << " =>\n\n";
         dest << "\t\t\t\t\t" << "case? x is" << endl;
@@ -257,13 +234,13 @@ void FSM2Process(int j, ofstream &dest)
 
                 string modified_y = stateProducts[k].y;
                 replace(modified_y.begin(), modified_y.end(), '-', OutputBitReplace);
-                
+
                 dest << "\t\t\t\t\t\t\t" << "y <= \"" << modified_y << "\";" << endl;
-                if(!isStringInVector(stateProducts[k].ns, cfsm[j])) // If the next state is not in the current cfsm
+                if (!isStringInVector(stateProducts[k].ns, cfsm[j])) // If the next state is not in the current cfsm
                 {
-                    dest << "\t\t\t\t\t\t\t" << "z("<< j <<") <= '0';" << endl; // Disable current cfsm clock
+                    dest << "\t\t\t\t\t\t\t" << "z(" << j << ") <= '0';" << endl; // Disable current cfsm clock
                     int z = find_cfsm(stateProducts[k].ns);
-                    dest << "\t\t\t\t\t\t\t" << "z("<< z <<") <= '1';" << endl; // Enable next cfsm clock
+                    dest << "\t\t\t\t\t\t\t" << "z(" << z << ") <= '1';" << endl; // Enable next cfsm clock
                 }
             }
         }
@@ -274,29 +251,33 @@ void FSM2Process(int j, ofstream &dest)
     dest << "\t\t\t" << "end case;" << endl;
 }
 
+void Fill_state_product(ifstream &source, ofstream &dest)
+{
 
-void Fill_state_product(ifstream &source, ofstream &dest) {
-    if (!source.is_open()) {
-        cerr << "Error: Unable to open source file." << endl;
-        return;
-    }
-
-    unordered_map<string, string> stateMap;  // To map states to state names
-      // To store state_product instances
+    unordered_map<string, string> stateMap; // To map states to state names
+                                            // To store state_product instances
     string line;
-    
+    vector<string> Test_Format_list;
+
+    for (int i = 0; i < states; ++i)
+    {
+        Test_Format_list.push_back("st" + to_string(i));
+    }
 
     source.clear();
     source.seekg(0, ios::beg);
 
     // First pass to find and handle the ".r" value
-    while (getline(source, line)) {
-        if (line.find(".r ") == 0) {
+    while (getline(source, line))
+    {
+        if (line.find(".r") != string::npos)
+        {
             istringstream iss(line);
             string temp;
-            iss >> temp >> Original_Reset_value;  // Read ".r" and its associated value
-            stateMap[Original_Reset_value] = "st0";  // Map the reset state to "st0"
-            break;  // We assume there's only one ".r" line
+            iss >> temp >> Original_Reset_value;    // Read ".r" and its associated value
+            stateMap[Original_Reset_value] = "st0"; // Map the reset state to "st0"
+            State_list.push_back("st0");
+            break; // We assume there's only one ".r" line
         }
     }
 
@@ -307,36 +288,63 @@ void Fill_state_product(ifstream &source, ofstream &dest) {
     source.seekg(0, ios::beg);
 
     // Second pass to process the actual state lines
-    while (getline(source, line)) {
+    while (getline(source, line))
+    {
         // Skip lines that start with a period or are empty
         if (line.empty() || line[0] == '.')
             continue;
 
         // Extract fields from the line
-        istringstream iss(line);
-        if (!(iss >> sp.x >> sp.cs >> sp.ns >> sp.y)) {
+        istringstream iss(line); // 0000 0101 1010 1111
+        if (!(iss >> sp.x >> sp.cs >> sp.ns >> sp.y))
+        { //      st5
             cerr << "Error: Failed to parse line: " << line << endl;
             continue;
         }
 
         // Map and replace cs if it does not start with 's'
-        if (sp.cs != "st0" && stateMap.find(sp.cs) == stateMap.end() && sp.cs[0] != 's') {
+        if (stateMap.find(sp.cs) == stateMap.end() )
+        {
+            if (!isStringInVector(sp.cs, Test_Format_list)){
             string stateName = "st" + to_string(stateMap.size());
             stateMap[sp.cs] = stateName;
-        }
-
-        if (sp.cs[0] != 's') {
+            State_list.push_back(stateName);
             sp.cs = stateMap[sp.cs];
+
+            }
+            else {
+                stateMap[sp.cs]=sp.cs;
+                State_list.push_back(sp.cs);
+            }
+           
         }
+        else {
+            sp.cs =  stateMap[sp.cs];
+        }
+        
+
+        // if (sp.cs[0] != 's') {
+      
+        //}
 
         // Map and replace ns if it does not start with 's'
-        if (sp.ns != "st0" && stateMap.find(sp.ns) == stateMap.end() && sp.ns[0] != 's') {
+         if (stateMap.find(sp.ns) == stateMap.end() )
+        {
+            if (!isStringInVector(sp.ns, Test_Format_list)){
             string stateName = "st" + to_string(stateMap.size());
             stateMap[sp.ns] = stateName;
-        }
-
-        if (sp.ns[0] != 's') {
+            State_list.push_back(stateName);
             sp.ns = stateMap[sp.ns];
+
+            }
+            else {
+                stateMap[sp.ns]=sp.ns;
+                State_list.push_back(sp.ns);
+            }
+           
+        }
+        else {
+            sp.ns =  stateMap[sp.ns];
         }
 
         // Add the state_product instance to the vector
@@ -345,25 +353,20 @@ void Fill_state_product(ifstream &source, ofstream &dest) {
         // Print the contents of the state_product instance
         cout << "x: " << sp.x << ", cs: " << sp.cs << ", ns: " << sp.ns << ",\ty: " << sp.y << endl;
     }
-
-   
 }
 
-
 // function to check if a string is in a vector of strings
-bool isStringInVector(const string& str, const vector<string>& vec) 
+bool isStringInVector(const string &str, const vector<string> &vec)
 {
     return find(vec.begin(), vec.end(), str) != vec.end();
 }
 
-
-
 // function to find which cfsm the state belongs to
 int find_cfsm(string state)
 {
-    for(int i = 0; i < cfsm.size(); i++)
+    for (int i = 0; i < cfsm.size(); i++)
     {
-        if(isStringInVector(state, cfsm[i]))
+        if (isStringInVector(state, cfsm[i]))
         {
             return i;
         }
@@ -416,85 +419,10 @@ void Optimiser_Axe(ifstream &source, vector<vector<string>> &cfsm)
     source.seekg(0, ios::beg);
 }
 
-void calculate_transition_probabilities(const vector<state_product>& stateProducts, map<pair<string, string>, double>& transitionProbs) {
-    // Initialize the transition counts
-    map<pair<string, string>, int> transitionCounts;
-    map<string, int> stateCounts;
-
-    // Count the transitions and occurrences of each state
-    for (const auto& sp : stateProducts) {
-        transitionCounts[{sp.cs, sp.ns}]++;
-        stateCounts[sp.cs]++;
-    }
-
-    // Calculate transition probabilities
-    for (const auto& count : transitionCounts) {
-        string cs = count.first.first;
-        string ns = count.first.second;
-        transitionProbs[{cs, ns}] = static_cast<double>(count.second) / stateCounts[cs];
-    }
+void calculate_transition_probabilities_V1(const vector<state_product> &stateProducts, map<pair<string, string>, double> &transitionProbs)
+{
 }
 
-
-void Optimiser_Min_trans_prob(ifstream &source, vector<vector<string>> &cfsm) {
-    if (!source.is_open()) {
-        cerr << "Error: Unable to open source file." << endl;
-        return;
-    }
-
-    map<pair<string, string>, double> transitionProbs;
-    calculate_transition_probabilities(stateProducts, transitionProbs);
-
-    vector<string> firstHalf;
-    vector<string> secondHalf;
-
-    // Naive partitioning strategy (This can be improved)
-    for (int i = 0; i < states; ++i) {
-        if (i < states / 2) {
-            firstHalf.push_back("st" + to_string(i));
-        } else {
-            secondHalf.push_back("st" + to_string(i));
-        }
-    }
-
-    bool improvement = true;
-    while (improvement) {
-        improvement = false;
-        for (const auto& state : firstHalf) {
-            double currentP12 = 0.0, currentP21 = 0.0;
-            double newP12 = 0.0, newP21 = 0.0;
-
-            // Calculate current probabilities
-            for (const auto& ns : secondHalf) {
-                currentP12 += transitionProbs[{state, ns}];
-                currentP21 += transitionProbs[{ns, state}];
-            }
-
-            // Simulate moving the state to the second half
-            for (const auto& ns : firstHalf) {
-                if (ns != state) {
-                    newP12 += transitionProbs[{state, ns}];
-                    newP21 += transitionProbs[{ns, state}];
-                }
-            }
-            for (const auto& ns : secondHalf) {
-                if (ns != state) {
-                    newP12 += transitionProbs[{state, ns}];
-                    newP21 += transitionProbs[{ns, state}];
-                }
-            }
-
-            // Check if moving the state reduces the transition probability
-            if (newP12 + newP21 < currentP12 + currentP21) {
-                firstHalf.erase(remove(firstHalf.begin(), firstHalf.end(), state), firstHalf.end());
-                secondHalf.push_back(state);
-                improvement = true;
-                break;
-            }
-        }
-    }
-
-    cfsm.push_back(firstHalf);
-    cfsm.push_back(secondHalf);
-    
+void Optimiser_Min_trans_prob(vector<vector<string>> &cfsm)
+{
 }
