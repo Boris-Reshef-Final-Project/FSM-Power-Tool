fce070b31883a5eef10ce3762a6d9e04a808990f
Reshef Schachter <sixsixbest@gmail.com>
2024-09-02 16:12:49 +0300

    Fixed Fail count in TB templates

--- Templates/tb_state_machine_not_optimised.vhd
+++ Templates/tb_state_machine_not_optimised.vhd
@@ -115,7 +115,7 @@ begin
     done <= true;
     report "Testbench finished" severity note;
     assert good_lines = test_array'length
-      report "Result:  Pass=" & to_string(good_lines) & LF & "         " & "Fail=" & to_string(test_array'length) severity warning;
+      report "Result:  Pass=" & to_string(good_lines) & LF & "         " & "Fail=" & to_string(test_array'length - good_lines) severity warning;
     assert good_lines /= test_array'length
       report "Result: All lines good." severity note;
     wait;
